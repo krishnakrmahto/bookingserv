a68da5f0434048e768a667a5986d1fdcedcaf281
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-12-27 19:26:50 +0530

    added validations to the dto classes

--- bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/dto/Address.java
+++ bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/dto/Address.java
@@ -1,19 +1,24 @@
 package com.paypal.bfs.test.bookingserv.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class Address {
 
+  @NotEmpty
   private String line1;
 
   private String line2;
 
+  @NotEmpty
   private String city;
 
+  @NotEmpty
   private String state;
 
   @JsonProperty("zip_code")
+  @NotEmpty
   private String zipCode;
 }
--- bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/dto/Booking.java
+++ bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/dto/Booking.java
@@ -3,6 +3,7 @@ package com.paypal.bfs.test.bookingserv.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.Date;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +16,10 @@ public class Booking {
 
   private Integer id;
 
+  @NotNull
   private String firstName;
 
+  @NotNull
   private String lastName;
 
   private Date dateOfBirth;
