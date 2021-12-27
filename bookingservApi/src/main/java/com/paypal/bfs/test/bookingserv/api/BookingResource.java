62c30e59d2f689f07e746617d5fc6afd2d9c190f
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-12-27 18:21:24 +0530

    added new endpoints as per problem statement

--- bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/api/BookingResource.java
+++ bookingservApi/src/main/java/com/paypal/bfs/test/bookingserv/api/BookingResource.java
@@ -1,11 +1,10 @@
 package com.paypal.bfs.test.bookingserv.api;
 
 import com.paypal.bfs.test.bookingserv.api.model.Booking;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 public interface BookingResource {
     /**
@@ -14,10 +13,9 @@ public interface BookingResource {
      * @param booking the booking object
      * @return the created booking
      */
-    @RequestMapping(value = "/v1/bfs/booking", method = RequestMethod.POST)
-    ResponseEntity<Booking> create(@RequestBody Booking booking);
+    @PostMapping("/v1/bfs/bookings")
+    Booking create(@RequestBody Booking booking);
 
-    // ----------------------------------------------------------
-    // TODO - add a new operation for Get All the bookings resource.
-    // ----------------------------------------------------------
+    @GetMapping("/v1/bfs/bookings")
+    List<Booking> getAllBookings();
 }
--- bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
+++ bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
@@ -2,14 +2,19 @@ package com.paypal.bfs.test.bookingserv.impl;
 
 import com.paypal.bfs.test.bookingserv.api.BookingResource;
 import com.paypal.bfs.test.bookingserv.api.model.Booking;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookingResourceImpl implements BookingResource {
 
     @Override
-    public ResponseEntity<Booking> create(Booking booking) {
+    public Booking create(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getAllBookings() {
         return null;
     }
 }
