eec64365913fb0aeb193ee94e8a6af74313229ab
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-12-27 18:48:36 +0530

    added booking entity, service, repository

--- bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
+++ bookingservImplementation/src/main/java/com/paypal/bfs/test/bookingserv/impl/BookingResourceImpl.java
@@ -2,19 +2,24 @@ package com.paypal.bfs.test.bookingserv.impl;
 
 import com.paypal.bfs.test.bookingserv.api.BookingResource;
 import com.paypal.bfs.test.bookingserv.api.model.Booking;
+import com.paypal.bfs.test.bookingserv.service.BookingService;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BookingResourceImpl implements BookingResource {
+
+    private final BookingService service;
 
     @Override
     public Booking create(Booking booking) {
-        return null;
+        return service.create(booking);
     }
 
     @Override
     public List<Booking> getAllBookings() {
-        return null;
+        return service.getAllBookings();
     }
 }
